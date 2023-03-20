716677ddb94cedae7c7be3c94571d2d8c6d57e86
sachin3825 <sachinkumawat55@gmail.com>
2023-03-20 19:21:16 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <div className='flex flex-col justify-center items-center w-[100vw] h-[100vh] bg-gray-200'>
       <div className='text-center flex justify-center items-center flex-col'>
-        <h1 className='text-4xl font-bold'>Our Testimonals</h1>
-        <div className='bg-violet-400 h-[4px] w-1/6 '></div>
+        <h1 className='text-4xl font-bold tracking-wider'>Our Testimonals</h1>
+        <div className='bg-violet-400 h-[4px] w-[150px]  '></div>
         <Testimonal reviews={reviews} />
       </div>
     </div>
